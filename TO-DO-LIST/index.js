0f70aca9f1109f832e8f2cd5fbb9d85d4376c116
NIKHILLLLLLLLLL__ <mishranirajk121@gmail.com>
2023-08-12 12:39:03 +0530

    [ADDED]added css,js,html files

---
+++ TO-DO-LIST/index.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("taskInput");
+    const addTaskButton = document.getElementById("addTaskButton");
+    const taskList = document.getElementById("taskList");
+
+    addTaskButton.addEventListener("click", function () {
+        const taskText = taskInput.value.trim();
+        if (taskText !== "") {
+            const li = document.createElement("li");
+            li.innerHTML = `
+                <span>${taskText}</span>
+                <button class="deleteButton">Delete</button>
+            `;
+            li.classList.add("fade-in");
+            taskList.appendChild(li);
+            taskInput.value = "";
+        }
+    });
+
+    taskList.addEventListener("click", function (event) {
+        if (event.target.classList.contains("deleteButton")) {
+            const li = event.target.parentElement;
+            li.classList.add("fade-out");
+            li.addEventListener("animationend", function () {
+                li.remove();
+            });
+        }
+    });
+});
